050a27b26cb5dd23c1f105b1eece79f18d0fd5e4
Vasco Monteiro <156311179+Apple-bed@users.noreply.github.com>
2024-03-13 20:55:50 +0000

    bscbjk

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,14 +7,14 @@ export default function Home() {
   return (
     <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: "url('/backg2.jpg')" }}>
       <div className="absolute top-0 left-0 right-0 flex flex-col  items-center justify-center h-full">
-        <img src="/logobranco.png" alt="logo" className="w-60 h-25" /> 
+        <img src="/logobranco.png" alt="logo" className="w-60 h-auto" /> 
         <p className="text-white text-xl font-bold">Sneakers Team</p> 
         <div>
           <img src="/peeps.png" className='w-60'/>
         </div>
       </div>
       <div className="flex justify-center">
-        <Navbar />
+        <Navbar/>
       </div>
     </div>
   );
