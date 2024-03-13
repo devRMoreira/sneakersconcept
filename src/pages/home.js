a2583983de9005a43151bcf0f6b925853d69317c
Vasco Monteiro <156311179+Apple-bed@users.noreply.github.com>
2024-03-13 22:02:29 +0000

    insidnai

--- src/pages/home.js
+++ src/pages/home.js
@@ -36,7 +36,7 @@ export default function Home() {
         <img src="/logobranco.png" alt="logo" className="w-60 h-auto" />
         <p className="text-white text-xl font-bold">Sneakers Team</p>
 
-        <div className="bg-stone-700 w-[300px] rounded-lg p-4 mt-8 ">
+        <div className="bg-stone-700 w-[350px] rounded-lg p-4 mt-8 ">
           <p className="text-white text-lg font-bold mb-4">Saldos</p>
 
           <div className="grid grid-cols-2 gap-4">
@@ -53,11 +53,13 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <div className="bg-stone-700  w-[350px] rounded-lg p-4 mt-8 ">
         <div className="grid grid-cols-2 gap-4">
 
           {produtos.produtos ? produtos.produtos.map((ele) => <Link href={`/produto/${ele._id}`}>
             <img src={ele.foto} className="w-[181px] h-[190px] rounded-lg" />
           </Link>) : undefined}
+        </div>
         </div>
 
       </div>
