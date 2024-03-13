8a559a8a397c355b93ba02990d9c45420281904f
Vasco Monteiro <156311179+Apple-bed@users.noreply.github.com>
2024-03-13 19:38:29 +0000

    iohouibjib

--- src/frontend/components/navbar.js
+++ src/frontend/components/navbar.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const BottomNavBar = () => {
     return (
-        <nav className="fixed bottom-0 left-0 w-full bg-gray-800 p-4 flex justify-around items-center">
+        <nav className="fixed bottom-0 left-0 w-full bg-white p-4 flex justify-around items-center z-50">
             <Link href="/page1">
             </Link>
             <Link href="/page2">
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,86 +1,17 @@
-const sapatilhaExemplo = {
-  _id: "65f1aea6e6a0e41cf6583939",
-  marca: "Nike",
-  modelo: "Court Vision Low Nn",
-  foto: "https://i.imgur.com/pXcFq8m.png",
-  preco: 39.99,
-  quantidade: [
-    {
-      "tamanho": 38,
-      "stock": 0
-    },
-    {
-      tamanho: 39,
-      "stock": 1
-    },
-    {
-      tamanho: 40,
-      stock: 15
-    },
-    {
-      tamanho: 41,
-      stock: 10
-    }
-  ]
-}
-
 import Link from 'next/link';
-import { Router } from 'next/router';
 import { useState } from 'react';
+import BottomNavBar from '@/frontend/components/navbar';
 
-export default function PedidoForm() {
-  const [formData, setFormData] = useState({
-    email: '',
-    nome: '',
-    idade: '',
-    localidade: '',
-    marca: '',
-    modelo: '',
-    cor: '',
-    tamanho: ''
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData);
-    setFormData({
-      email: '',
-      nome: '',
-      idade: '',
-      localidade: '',
-      marca: '',
-      modelo: '',
-      cor: '',
-      tamanho: ''
-    });
-  };
-
-  /*  if (!event) {
-       return (
-         <div>
-           <div className="flex justify-center items-center h-screen ">
-             <img
-               src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/f9aea342094811.57c019c4f089a.gif"
-               alt="Loading"
-             />
-           </div>
-         </div>
-       );
-     } */
-
+export default function Home() {
   return (
-    <div className="container mx-auto p-4">
-      
-      <p>HomePage</p>
-      
+    <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: "url('/backg2.jpg')" }}>
+      <div className="absolute top-8 left-1/2 transform -translate-x-1/2">
+        <img src="/logobranco.png" alt="logo" className="w-68" />
+      </div>
+
+      <div className="flex justify-center">
+        <BottomNavBar />
+      </div>
     </div>
   );
 }
