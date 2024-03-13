25e0a2ddb40d43a37b09fdc750afcf508f0a3fd2
Vasco Monteiro <156311179+Apple-bed@users.noreply.github.com>
2024-03-13 21:18:13 +0000

    INSCIOANLKNX

--- src/pages/home.js
+++ src/pages/home.js
@@ -9,7 +9,7 @@ export default function Home() {
         <img src="/logobranco.png" alt="logo" className="w-60 h-auto" /> 
         <p className="text-white text-xl font-bold">Sneakers Team</p>
         
-        <div className="bg-blue-500 w-[300px] rounded-lg p-4 mt-8 ">
+        <div className="bg-stone-700 w-[300px] rounded-lg p-4 mt-8 ">
           <p className="text-white text-lg font-bold mb-4">Saldos</p>
           
           <div className="grid grid-cols-2 gap-4">
@@ -21,7 +21,7 @@ export default function Home() {
             </div>
           </div>
         </div>
-        
+                
       </div>
       
     </div>
