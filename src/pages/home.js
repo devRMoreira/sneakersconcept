69eb4bc530f6edeff6bc5de12813a35e8225cd4d
Vasco Monteiro <156311179+Apple-bed@users.noreply.github.com>
2024-03-13 21:12:48 +0000

    scianis

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,21 +1,29 @@
 import Navbar from '@/frontend/components/navbar';
 import Link from 'next/link';
 import { useState } from 'react';
-import { Inter } from "next/font/google";
 
 export default function Home() {
   return (
     <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: "url('/backg2.jpg')" }}>
-      <div className="absolute top-0 left-0 right-0 flex flex-col  items-center justify-center h-full">
+      <div className="absolute top-0 left-0 right-0 flex flex-col items-center justify-center h-full">
         <img src="/logobranco.png" alt="logo" className="w-60 h-auto" /> 
-        <p className="text-white text-xl font-bold">Sneakers Team</p> 
-        <div>
-          <img src="/peeps.png" className='w-60'/>
+        <p className="text-white text-xl font-bold">Sneakers Team</p>
+        
+        <div className="bg-blue-500 w-[300px] rounded-lg p-4 mt-8 ">
+          <p className="text-white text-lg font-bold mb-4">Saldos</p>
+          
+          <div className="grid grid-cols-2 gap-4">
+            <div className="bg-white rounded-lg overflow-hidden">
+              <img src="/sneaker1.jpg" className="w-full" alt="Sneaker 1" />
+            </div>
+            <div className="bg-white rounded-lg overflow-hidden">
+              <img src="/sneaker2.jpg" className="w-full" alt="Sneaker 2" />
+            </div>
+          </div>
         </div>
+        
       </div>
-      <div className="flex justify-center">
-        <Navbar/>
-      </div>
+      
     </div>
   );
 }
