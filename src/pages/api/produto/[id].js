bd9694c10085e0902a0361d77ef1ff17edd9a78e
RM <devRMoreira@gmail.com>
2024-03-13 19:41:22 +0000

    Implementada lógica para flash sales não duplicarem

--- src/pages/api/produto/[id].js
+++ src/pages/api/produto/[id].js
@@ -1,9 +1,10 @@
-import { getFlashSales, getProduto } from "@/backend/data/produto"
+import { getProduto, getTodosProdutos } from "@/backend/data/produto"
+import { getFlashSales } from "@/backend/services/produto"
 
 export default async function handler(req, res) {
 
   if (req.method === "GET") {
-    
+
     const id = req.query.id
 
     if (id === "flash") {
@@ -14,11 +15,19 @@ export default async function handler(req, res) {
 
     }
 
+    if (id === "todos") {
+
+      const produtos = await getTodosProdutos()
+
+      return res.status(200).json(produtos)
+
+    }
+
 
     const produto = await getProduto(id)
 
     return res.status(200).json(produto)
-    
+
   } else {
 
     return res.status(404).json(undefined)
