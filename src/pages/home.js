4938f815ccc0c66a45b9c55dee1df41e1139baf4
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-13 14:55:51 +0000

    form  with button

--- src/pages/home.js
+++ src/pages/home.js
@@ -23,3 +23,64 @@ const sapatilhaExemplo = {
     }
   ]
 }
+
+import Link from 'next/link';
+import { Router } from 'next/router';
+import { useState } from 'react';
+
+export default function PedidoForm() {
+  const [formData, setFormData] = useState({
+    email: '',
+    nome: '',
+    idade: '',
+    localidade: '',
+    marca: '',
+    modelo: '',
+    cor: '',
+    tamanho: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    setFormData({
+      email: '',
+      nome: '',
+      idade: '',
+      localidade: '',
+      marca: '',
+      modelo: '',
+      cor: '',
+      tamanho: ''
+    });
+  };
+
+  /*  if (!event) {
+       return (
+         <div>
+           <div className="flex justify-center items-center h-screen ">
+             <img
+               src="https://mir-s3-cdn-cf.behance.net/project_modules/disp/f9aea342094811.57c019c4f089a.gif"
+               alt="Loading"
+             />
+           </div>
+         </div>
+       );
+     } */
+
+  return (
+    <div className="container mx-auto p-4">
+      {/* top bar com logo e search sneakers */}
+      <p>HomePage</p>
+      {/* navbar */}
+    </div>
+  );
+}
