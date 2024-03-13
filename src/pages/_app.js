5cfcf214a6c3fd35584ba48352d58215aa17dad6
RM <devRMoreira@gmail.com>
2024-03-13 19:03:59 +0000

    implementada navbar permanente

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,33 @@
+import BottomNavBar from "@/frontend/components/navbar";
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
+export const toastSucesso = (string) => toast.success(string)
+export const toastErro = (string) => toast.error(string)
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  const router = useRouter()
+  const { asPath, route, pathname } = router
+
+  if (asPath === "/") {
+    return <div>
+
+      <Component {...pageProps} />;
+
+    </div>
+
+  } else {
+
+    return <div>
+
+      <ToastContainer autoClose={2500} />
+      <Component {...pageProps} />
+      <BottomNavBar />
+
+    </div>
+  }
 }
