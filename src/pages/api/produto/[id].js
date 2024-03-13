64421b65523aad3b05745290e7d44d0dce2ddb13
RM <devRMoreira@gmail.com>
2024-03-13 14:48:35 +0000

    init mongodb

--- src/pages/api/produto/[id].js
+++ src/pages/api/produto/[id].js
@@ -1,6 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    res.status(200).json({ name: "John Doe" });
+
+  if(req.method === "GET"){
+
+    const id = req.query.id
   }
-  
+
+}
