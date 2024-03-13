0c4190a365167aabd6ec48b139151a5553e6098a
vasco2024 <156312623+vasco2024@users.noreply.github.com>
2024-03-13 22:17:14 +0000

    xws

---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import React from "react";
+
+const about = () => {
+    return (
+        <div className="p-4">
+            <Image
+                width={500}
+                height={100}
+                alt="Sneakers Concept"
+                src={"/logopreto.png"}
+            />
+            <section className="mx-auto max-w-[700px] text-xl">
+                <p className="mt-2 mb-6">
+                    <b>Sneakers Concept</b> é uma loja de calçado especializada em sapatilhas, localizada no encantador bairro de Aldoar, no Porto, Portugal. A nossa paixão pelas sapatilhas vai além do simples calçado; é uma verdadeira cultura que celebramos e partilhamos com a nossa comunidade.
+                </p>
+                <p className="mb-6">
+                    Na nossa loja, irá encontrar uma seleção cuidada das melhores marcas e modelos de sapatilhas de todo o mundo. Desde os clássicos intemporais até às últimas novidades da moda, temos tudo o que necessita para expressar o seu estilo único e autêntico.
+                </p>
+                <p className="mb-6">
+                    A nossa equipa apaixonada está sempre pronta para o ajudar a encontrar o par perfeito de sapatilhas que se ajuste não apenas ao seu estilo, mas também ao seu conforto e desempenho. Valorizamos a qualidade, o conforto e o estilo, e acreditamos que cada pessoa merece calçado que os faça sentir confiantes e inspirados.
+                </p>
+                <p className="mb-6">
+                    Além da nossa loja física em Aldoar, também oferecemos uma experiência de compra online conveniente, para que possa explorar a nossa coleção e fazer as suas compras a qualquer hora e em qualquer lugar.
+                </p>
+                <p className="mb-6">
+                    Na Sneakers Concept, estamos comprometidos em fornecer não apenas os melhores produtos, mas também um serviço excepcional. Estamos aqui para tornar a sua experiência de compra de sapatilhas verdadeiramente memorável e satisfatória.
+                </p>
+                <h3 className="underline font-semibold my-6">Missão</h3>
+                <p className="mb-6">
+                    A nossa missão na Sneakers Concept é simples: oferecer a melhor seleção de sapatilhas de qualidade, combinada com um serviço excepcional, para que os nossos clientes possam encontrar calçado que os inspire e os capacite a alcançar os seus objetivos, seja na moda, no desporto ou na vida quotidiana.
+                </p>
+            </section>
+        </div>
+    );
+};
+
+export default about;
--- src/pages/formulario.js
+++ src/pages/formulario.js
@@ -45,91 +45,94 @@ export default function PedidoForm() {
     };
 
     return (
-        <div className="container mx-auto p-4">
-            <form onSubmit={handleSubmit} className="max-w-md mx-auto">
-                <div>
-                    <p>Tens um modelo específico de sapatilhas em mente?</p>
-                    <div
-                        onClick={handleToggleInputs}
-                        style={{
-                            cursor: 'pointer',
-                            backgroundColor: showInputs ? '#e5e5e5' : 'transparent',
-                            padding: '0.5rem',
-                            borderRadius: '0.5rem'
-                        }}
-                    >
-                        Não sabes o que queres?
+        <div className="relative bg-cover bg-center h-screen" style={{ backgroundImage: "url('/backg2.jpg')" }}>
+
+            <div className="container mx-auto p-4">
+                <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+                    <div>
+                        <h1 className="block mb-2 text-xl font-bold text-white mx-auto text-center">Tens um modelo específico de sapatilhas em mente? Diz-nos qual é!</h1>
+                        <div
+                            onClick={handleToggleInputs}
+                            style={{
+                                cursor: 'pointer',
+                                backgroundColor: showInputs ? '#e5e5e5' : 'transparent',
+                                padding: '0.5rem',
+                                borderRadius: '0.5rem'
+                            }}
+                        >
+                            <h2 className="block mb-2 text-white"> Não sabes o que queres?</h2>
+                        </div>
                     </div>
+                    {showInputs && (
+                        <div>
+                            <label className="block mb-2 text-white">
+                                Email:
+                                <input type="email" name="email" value={formData.email} onChange={handleChange} /* required */ className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Nome:
+                                <input type="text" name="nome" value={formData.nome} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Idade:
+                                <input type="number" name="idade" value={formData.idade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Localidade:
+                                <input type="text" name="localidade" value={formData.localidade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Marca:
+                                <input type="text" name="marca" value={formData.marca} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Modelo:
+                                <input type="text" name="modelo" value={formData.modelo} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Cor:
+                                <input type="text" name="cor" value={formData.cor} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2">
+                                Tamanho:
+                                <input type="number" name="tamanho" value={formData.tamanho} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                        </div>
+                    )}
+                    {!showInputs && (
+                        <div>
+                            <label className="block mb-2 text-white">
+                                Email:
+                                <input type="email" name="email" value={formData.email} onChange={handleChange} /* required */ className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Nome:
+                                <input type="text" name="nome" value={formData.nome} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Idade:
+                                <input type="number" name="idade" value={formData.idade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                Localidade:
+                                <input type="text" name="localidade" value={formData.localidade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 text-black" />
+                            </label>
+                            <label className="block mb-2 text-white">
+                                <p>Por favor, descreve as sapatilhas:</p>
+                                <textarea name="descricao" value={formData.descricao} onChange={handleChange} placeholder="Descreve as sapatilhas aqui" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 " />
+                            </label>
+                        </div>
+                    )}
+                    <button type="submit" className="bg-amber-950 w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brown-700">Enviar Pedido</button>
+                </form>
+                <div className="my-4">
+                    <h2 className="text-lg font-semibold mb-2">Form Data</h2>
+                    <pre>{JSON.stringify(formData, null, 2)}</pre>
                 </div>
-                {showInputs && (
-                    <div>
-                        <label className="block mb-2">
-                            Email:
-                            <input type="email" name="email" value={formData.email} onChange={handleChange} /* required */ className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Nome:
-                            <input type="text" name="nome" value={formData.nome} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Idade:
-                            <input type="number" name="idade" value={formData.idade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Localidade:
-                            <input type="text" name="localidade" value={formData.localidade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Marca:
-                            <input type="text" name="marca" value={formData.marca} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Modelo:
-                            <input type="text" name="modelo" value={formData.modelo} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Cor:
-                            <input type="text" name="cor" value={formData.cor} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Tamanho:
-                            <input type="number" name="tamanho" value={formData.tamanho} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                    </div>
-                )}
-                {!showInputs && (
-                    <div>
-                        <label className="block mb-2">
-                            Email:
-                            <input type="email" name="email" value={formData.email} onChange={handleChange} /* required */ className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Nome:
-                            <input type="text" name="nome" value={formData.nome} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Idade:
-                            <input type="number" name="idade" value={formData.idade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            Localidade:
-                            <input type="text" name="localidade" value={formData.localidade} onChange={handleChange} placeholder="Opcional" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                        <label className="block mb-2">
-                            <p>Por favor, descreve as sapatilhas:</p>
-                            <textarea name="descricao" value={formData.descricao} onChange={handleChange} placeholder="Descreve as sapatilhas aqui" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                        </label>
-                    </div>
-                )}
-                <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Enviar Pedido</button>
-            </form>
-            <div className="my-4">
-                <h2 className="text-lg font-semibold mb-2">Form Data</h2>
-                <pre>{JSON.stringify(formData, null, 2)}</pre>
+                <Link href="/home">
+                    <p>De volta para a página inicial</p>
+                </Link>
             </div>
-            <Link href="/home">
-                <p>De volta para a página inicial</p>
-            </Link>
         </div>
     );
 }
