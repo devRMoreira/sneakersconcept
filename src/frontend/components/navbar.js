b94496069ab67637289af94331fe2bddce280d83
Vasco Monteiro <156311179+Apple-bed@users.noreply.github.com>
2024-03-14 00:28:54 +0000

    ioshiofah

--- src/frontend/components/navbar.js
+++ src/frontend/components/navbar.js
@@ -1,32 +1,25 @@
-// components/BottomNavBar.js
-import Link from 'next/link';
-import React from 'react';
 import { PiSneakerFill } from "react-icons/pi";
 import { IoSearch } from "react-icons/io5";
 import { MdShoppingCart } from "react-icons/md";
 
-
-
-
-const NavBar = () => {
+const BottomNavBar = () => {
   return (
-
-
-    <div className="flex items-center space-x-4 p-4 bg-white relative ">
-      <a href="./home" className="text-black flex items-center">
-        <PiSneakerFill />
-      </a>
-      <a href="" className="text-whiteflex items-center">
-        <IoSearch className="mr-2" />
-      </a>
-      <a href="" className="text- flex items-center">
-        <MdShoppingCart className="mr-2" />
-      </a>
-
+    <div className="relative">
+      <div className="sticky bottom-0 left-0 right-0 bg-white flex items-center p-4">
+        <div className="flex justify-between w-full">
+          <a href="/home" className="text-black flex items-center">
+            <PiSneakerFill />
+          </a>
+          <a href="#" className="text-black flex items-center ml-auto">
+            <IoSearch className="mr-2" />
+          </a>
+          <a href="#" className="text-black flex items-center">
+            <MdShoppingCart className="mr-2" />
+          </a>
+        </div>
+      </div>
     </div>
-
-
   );
 };
 
-export default NavBar;
+export default BottomNavBar;
